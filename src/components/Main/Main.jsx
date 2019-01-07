fe1de0c2876d8a448b8dfbc8dfb22aa39dec8bf0
Andrew John <andrew.john.nls@gmail.com>
2019-01-07 19:00:33 +0100

    implemening new Main design

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,44 @@
 // @flow
 import React from 'react';
+import './Main.css';
+import ProjectListDetail from '../ProjectListDetail';
+
+const projects = [
+  {
+    id: 1,
+    title: 'Voetbalprimeur',
+    links: ['https://voetbalprimeur.nl'],
+  },
+  {
+    id: 2,
+    title: 'GPBlog',
+    links: ['https://gpblog.com'],
+  },
+  {
+    id: 3,
+    title: 'Chepicap',
+    links: ['https://chepicap.com'],
+  },
+  {
+    id: 4,
+    title: 'Haringvliet',
+    links: [],
+  },
+  {
+    id: 5,
+    title: 'BabyBytes App',
+    links: [
+      'https://play.google.com/store/apps/details?id=com.de.babybytesvoeding',
+      'https://itunes.apple.com/nl/app/zwangerschaps-voedingswijzer/id1248716004?mt=8',
+    ],
+  },
+];
 
 const Main = () => (
-  <div>
-    <h1>Main</h1>
+  <div className="Main">
+    { projects.map(project => (
+      <ProjectListDetail key={project.id} {...project} />
+    ))}
   </div>
 );
 
