6250766a21cb855ecd7c1b2f5eb40f66b8373bd5
Andrew John <andrew.john.nls@gmail.com>
2019-01-09 16:52:03 +0100

    add new properies to project data

--- src/components/ProjectListDetail/ProjectListDetail.jsx
+++ src/components/ProjectListDetail/ProjectListDetail.jsx
@@ -4,21 +4,35 @@ import './ProjectListDetail.css';
 
 type Props = {
   title: string,
+  text: string,
+  skills: Array<string>,
   links: Array<string>,
 };
 
-const ProjectListDetail = ({ title, links }: Props) => (
+const ProjectListDetail = ({ title, links, text, skills }: Props) => (
   <div className="ProjectListDetail">
     <div className="Title">
       <h2>{title}</h2>
     </div>
-    <ul>
-      { links.length > 0 && links.map(link => (
-        <li key={link}>
-          <a href="{link}">{link}</a>
-        </li>
-      ))}
-    </ul>
+    <p>{text}</p>
+    <div className="Skills">
+      <h4>Skills</h4>
+      <ul>
+        { skills.length > 0 && skills.map(skill => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+    <div className="links">
+      <h4>Links</h4>
+      <ul>
+        { links.length > 0 && links.map(link => (
+          <li key={link}>
+            <a href={link} target="_blank">{link}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   </div>
 );
 
