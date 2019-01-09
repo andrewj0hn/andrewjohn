393732726c0c5080ac558c2315c4bb91aa44043a
Andrew John <andrew.john.nls@gmail.com>
2019-01-09 16:47:55 +0100

    add react router dom package

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,15 @@
 // @flow
 import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import './App.css';
 import './Fonts.css';
 import Header from '../Header';
 import Main from '../Main';
 import Footer from '../Footer';
+
+const AppNavigator = () => createStackNavigator({
+  Home: 
+});
 
 const App = () => (
   <div className="App">
