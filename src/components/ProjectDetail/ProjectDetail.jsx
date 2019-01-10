bebb3d8c266a14f43145bec1c756edf699a8b913
Andrew John <andrew.john.nls@gmail.com>
2019-01-10 18:06:08 +0100

    start setup project detail page

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,8 +1,45 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
+import projects from '../../data/projects';
 
-const ProjectDetail = () => (
-  <div />
-);
+type Props = {
+  match: Object,
+}
+
+type State = {
+  title: string,
+  images: Array<string>,
+}
+
+class ProjectDetail extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const project = projects.find(project => project.id == id);
+    this.setState({ ...project });
+    document.title = project.title;
+  }
+
+  render() {
+    const { title, images } = this.state;
+    return (
+      <div>
+        <div className="Image">
+          { images.map(image => (
+            <img src={image} alt={title} />
+          )) }
+        </div>
+      </div>
+    )
+  }
+}
 
 export default ProjectDetail;
