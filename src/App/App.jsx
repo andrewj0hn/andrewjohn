1444d00d6678a590c51745ed44e5d34a7da7d710
Andrew John <dev@andrewjohn.nl>
2019-02-02 14:04:26 +0100

    edit app imports

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from '../components/Header';
-import ProjectList from '../components/ProjectList';
-import ProjectDetail from '../components/ProjectDetail';
+import ListOverview from '../components/ListOverview';
+import Detail from '../components/Detail';
 import Contact from '../components/Contact';
 import * as ROUTES from '../constants/Routes';
 
@@ -13,11 +13,11 @@ const App = () => (
     <div className="App">
       <Header />
       <div className="Main">
-        <Route exact path={ROUTES.HOME} component={ProjectList} />
-        <Route path={ROUTES.DIGITAL_ENTERPRISES} component={ProjectList} />
-        <Route path={ROUTES.HOGESCHOOL_VAN_AMSTERDAM} component={ProjectList} />
-        <Route path={ROUTES.OWN_WORK} component={ProjectList} />
-        <Route path={`${ROUTES.DETAIL}/:id`} component={ProjectDetail} />
+        <Route exact path={ROUTES.HOME} component={ListOverview} />
+        <Route path={ROUTES.DIGITAL_ENTERPRISES} component={ListOverview} />
+        <Route path={ROUTES.HOGESCHOOL_VAN_AMSTERDAM} component={ListOverview} />
+        <Route path={ROUTES.OWN_WORK} component={ListOverview} />
+        <Route path={`${ROUTES.DETAIL}/:id`} component={Detail} />
         <Route path={ROUTES.CONTACT} component={Contact} />
       </div>
     </div>
